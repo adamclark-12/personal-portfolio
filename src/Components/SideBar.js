8b62824cef4296a168d9ab52a12945598bd17df5
LF <adam-c1991@hotmail.com>
2022-05-10 17:01:35 +0100

    complete visuals for sidebar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,23 +2,53 @@ import{BiHomeSmile} from "react-icons/bi";
 import {GrMail} from "react-icons/gr";
 import {FaGithub, FaUser} from "react-icons/fa";
 const SideBar = () => {
-
     return(
-<div class=" text-slate-500 dark:bg-slate-600 dark:text-slate-200 shadow-xl shadow-sky-800
-             top-2 left-1 flex flex-row items-center space-x-20 w-screen p-10 m-6 text-4xl">
+        <div class=" text-slate-500 dark:bg-slate-600 dark:text-slate-200 shadow-xl shadow-sky-800
+             top-2 flex flex-row items-center space-x-20 w-screen p-10 m-0 text-4xl fixed">
 
-            {<SideBarIcon  icon={<BiHomeSmile size ="40" />} /> /*  home button */}
-            { <SideBarIcon icon={<FaUser size ="40" />} />    /*about me button */}
-            { <SideBarIcon icon={<FaGithub size ="40" />} /> /* github button */}
-            {<SideBarIcon icon={<GrMail size ="40" />} />    /* contacts button */}
-            <h1>This piece of shit</h1>
+            {<SideBarIcon_1 icon={<BiHomeSmile size ="40" />} /> /*  home button */}
+            {<SideBarIcon_2 icon={<FaUser size ="40" />} />    /*about me button */}
+            {<SideBarIcon_3 icon={<FaGithub size ="40" />} /> /* github button */}
+            {<SideBarIcon_4 icon={<GrMail size ="40" />} />    /* contacts button */}
+            <h1>Hello there</h1>
         </div>
     )
 };
 
-const SideBarIcon = ({icon}) => (
-    <div className="sidebar-icon">
+const SideBarIcon_1 = ({icon, text = 'tooltip'}) => (
+    <div className="sidebar-icon group">
         {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {"Home"}
+        </span>
+    </div>
+)
+const SideBarIcon_2 = ({icon, text = 'tooltip'}) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {"About Me"}
+        </span>
+    </div>
+)
+const SideBarIcon_3 = ({icon, text = 'tooltip'}) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {"GitHub Page"}
+        </span>
+    </div>
+)
+const SideBarIcon_4 = ({icon, text = 'tooltip'}) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {"Contact"}
+        </span>
     </div>
 )
 export default SideBar;
