be0a83100643570855857cfc186fc356eee04f9b
LF <adam-c1991@hotmail.com>
2022-09-15 16:15:16 +0100

    contact page message design improvment & back button added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { SiMinutemailer } from "react-icons/si";
 import { useForm, ValidationError } from "@formspree/react";
+import { useNavigate } from "react-router";
+import { AiOutlineRollback } from "react-icons/ai";
 
 const Contact = () => {
+  const navigate = useNavigate();
   const [state, handleSubmit] = useForm("mvoygyjp");
   if (state.succeeded) {
-    return <p className="">Thanks for joining!</p>;
+    return (
+      <p className="text-slate-200 text-center content-center font-pFont text-xl mt-40">
+        Thank you for getting in touch, I will get back to you as soon as I can!
+        <br />
+        <br />
+        <button
+          className=" shadow-lg
+          hover:text-gray-700 hover:bg-[#F7B023] rounded-3xl hover:rounded-xl transition-all duration-200
+          ease-linear cursor-pointer;"
+          onClick={() => navigate(-1)}
+        >
+          Go Home <AiOutlineRollback className="inline " />
+        </button>
+      </p>
+    );
   }
   return (
     <form className="flex flex-col my-2 pt-10 " onSubmit={handleSubmit}>
       <div className="absolute top-48 overflow-y-auto bottom-0 left-0 right-0 text-slate-200">
         <h1 className="font-h1Font text-8xl text-center">Contact page</h1>
-        <p className="font-pFont text-sm italic ml-[37rem]">
+        <p className="font-pFont text-sm italic text-center">
           Please fill out all fields.
         </p>
 
